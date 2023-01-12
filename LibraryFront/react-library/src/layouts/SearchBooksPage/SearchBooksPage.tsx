79144699fec7844a7c7e358b5894e92d551672b2
minvoo <maciejewski_mariusz@wp.pl>
2023-01-12 21:28:24 +0100

    unfinished pagination

--- LibraryFront/react-library/src/layouts/SearchBooksPage/SearchBooksPage.tsx
+++ LibraryFront/react-library/src/layouts/SearchBooksPage/SearchBooksPage.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import BookModel from "../../models/BookModel";
+import { Pagination } from "../Utils/Pagination";
 import { SpinnerLoading } from "../Utils/SpinnerLoading";
 import { SearchBook } from "./SearchBook";
 
@@ -7,11 +8,17 @@ export const SearchBooksPage = () => {
   const [books, setBooks] = useState<BookModel[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [httpError, setHttpError] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [booksperPage, setBooksPerPage] = useState(5);
+  const [totalAmountOfBooks, setTotalAmountOfBooks] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
 
   useEffect(() => {
     const fetchBooks = async () => {
       const baseUrl: string = "http://localhost:8080/api/books";
-      const url: string = `${baseUrl}?page=0&size=5`;
+      const url: string = `${baseUrl}?page=${
+        currentPage - 1
+      }&size=${booksperPage}`;
 
       const response = await fetch(url);
 
@@ -21,6 +28,9 @@ export const SearchBooksPage = () => {
 
       const responseJson = await response.json();
       const responseData = responseJson._embedded.books;
+
+      setTotalAmountOfBooks(responseJson.page.totalElements);
+      setTotalPages(responseJson.page.totalPages);
 
       const loadedBooks: BookModel[] = [];
 
@@ -59,6 +69,14 @@ export const SearchBooksPage = () => {
     );
   }
 
+  const indexOfLastBook: number = currentPage * booksperPage;
+  const indexOfFirstBook: number = indexOfLastBook - booksperPage;
+  let lastItem =
+    booksperPage * currentPage <= totalAmountOfBooks
+      ? booksperPage * currentPage
+      : totalAmountOfBooks;
+
+  const paginate = (pageNumber: number) => setCurrentPage;
   return (
     <div>
       <div className="container ">
@@ -75,50 +93,63 @@ export const SearchBooksPage = () => {
             </div>
           </div>
           <div className="col-4">
-            <div  className="dropdown">
-                <button className="btn btn-secondary dropdown-toggle" type="button"
-                id="dropdownMenuButton1" data-bs-toggle='dropdown' aria-expanded='false'>
-                    Category
-                </button>
-                <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                    <li>
-                        <a href="#" className="dropdown-item">
-                            All
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" className="dropdown-item">
-                            Frontend
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" className="dropdown-item">
-                            Backend
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" className="dropdown-item">
-                            Data
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" className="dropdown-item">
-                            DevOps
-                        </a>
-                    </li>
-                </ul>
+            <div className="dropdown">
+              <button
+                className="btn btn-secondary dropdown-toggle"
+                type="button"
+                id="dropdownMenuButton1"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Category
+              </button>
+              <ul
+                className="dropdown-menu"
+                aria-labelledby="dropdownMenuButton1"
+              >
+                <li>
+                  <a href="#" className="dropdown-item">
+                    All
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="dropdown-item">
+                    Frontend
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="dropdown-item">
+                    Backend
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="dropdown-item">
+                    Data
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="dropdown-item">
+                    DevOps
+                  </a>
+                </li>
+              </ul>
             </div>
           </div>
           <div className="mt-3">
             <h5>Number of results: (22)</h5>
           </div>
-          <p>
-            1 to 5 of 22 items:
-          </p>
-          {books.map(book => (
+          <p>1 to 5 of 22 items:</p>
+          {books.map((book) => (
             <SearchBook book={book} key={book.id} />
           ))}
         </div>
+        {totalPages > 1 && (
+          <Pagination
+            currentPage={currentPage}
+            totalPages={totalPages}
+            paginate={paginate}
+          />
+        )}
       </div>
     </div>
   );
