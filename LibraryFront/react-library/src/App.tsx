c3af186282e318a830433b9ac46a3997202447ac
minvoo <maciejewski_mariusz@wp.pl>
2023-01-14 21:19:38 +0100

    footer sticks to bottom of the page

--- LibraryFront/react-library/src/App.tsx
+++ LibraryFront/react-library/src/App.tsx
@@ -3,18 +3,26 @@ import { Footer } from "./layouts/Footer/Footer";
 import { Navbar } from "./layouts/Navbar/Navbar";
 import { HomePage } from "./layouts/HomePage/HomePage";
 import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch, Redirect } from "react-router-dom";
 
 export const App = () => {
   return (
-    <>
+    <div className="d-flex flex-column min-vh-100">
       <Navbar />
-      <Switch>
-      <Route path='/' exact><Redirect to ='/home'/></Route>
-      <Route path='/home' exact><HomePage /></Route>
-      <Route path='/search'><SearchBooksPage /></Route>
-      </Switch>
+      <div className="flex-grow-1">
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/home" />
+          </Route>
+          <Route path="/home" exact>
+            <HomePage />
+          </Route>
+          <Route path="/search">
+            <SearchBooksPage />
+          </Route>
+        </Switch>
+      </div>
       <Footer />
-    </>
+    </div>
   );
-}
+};
