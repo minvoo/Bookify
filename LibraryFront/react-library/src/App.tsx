8fdb6933c18d145e56f2d3ce97f68cd267aaaa4a
minvoo <maciejewski_mariusz@wp.pl>
2023-01-15 18:02:12 +0100

    authentication implement using okta.com

--- LibraryFront/react-library/src/App.tsx
+++ LibraryFront/react-library/src/App.tsx
@@ -1,32 +1,62 @@
-import "./App.css";
-import { Footer } from "./layouts/Footer/Footer";
-import { Navbar } from "./layouts/Navbar/Navbar";
-import { HomePage } from "./layouts/HomePage/HomePage";
-import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
-import { Route, Switch, Redirect } from "react-router-dom";
-import { BookCheckoutPage } from "./layouts/BookCheckoutPage/BookCheckoutPage";
+import React from 'react';
+import { Redirect, Route, Switch, useHistory } from 'react-router-dom';
+import './App.css';
+import { BookCheckoutPage } from './layouts/BookCheckoutPage/BookCheckoutPage';
+import { HomePage } from './layouts/HomePage/HomePage';
+import { Footer } from './layouts/Footer/Footer';
+import { Navbar } from './layouts/Navbar/Navbar';
+import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
+import { oktaConfig } from './lib/oktaConfig';
+import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
+import { Security, LoginCallback, SecureRoute } from '@okta/okta-react';
+import LoginWidget from './Auth/LoginWidget';
+
+
+const oktaAuth = new OktaAuth(oktaConfig);
 
 export const App = () => {
+
+  const customAuthHandler = () => {
+    history.push('/login');
+  }
+
+  const history = useHistory();
+
+  const restoreOriginalUri = async (_oktaAuth: any, originalUri: any) => {
+    history.replace(toRelativeUrl(originalUri || '/', window.location.origin));
+  };
+
+
   return (
-    <div className="d-flex flex-column min-vh-100">
+    <div className='d-flex flex-column min-vh-100'>
+      <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri} onAuthRequired={customAuthHandler}>
       <Navbar />
-      <div className="flex-grow-1">
+      <div className='flex-grow-1'>
         <Switch>
-          <Route path="/" exact>
-            <Redirect to="/home" />
+          <Route path='/' exact>
+            <Redirect to='/home' />
           </Route>
-          <Route path="/home" exact>
+          <Route path='/home'>
             <HomePage />
           </Route>
-          <Route path="/search">
+          <Route path='/search'>
             <SearchBooksPage />
           </Route>
-          <Route path="/checkout/:bookId">
-            <BookCheckoutPage />
+          <Route path='/reviewlist/:bookId'>
+
           </Route>
+          <Route path='/checkout/:bookId'>
+            <BookCheckoutPage/>
+          </Route>
+          <Route path='/login' render={
+            () => <LoginWidget config={oktaConfig} /> 
+            } 
+          />
+          <Route path='/login/callback' component={LoginCallback} />
         </Switch>
       </div>
       <Footer />
+      </Security>
     </div>
   );
-};
+}
