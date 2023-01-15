2dd8306f4e0eadc9d231214cb6b967e7054bb4d3
minvoo <maciejewski_mariusz@wp.pl>
2023-01-15 13:54:22 +0100

    okta config first step

---
+++ LibraryFront/react-library/src/lib/oktaConfig.ts
@@ -0,0 +1,8 @@
+export const oktaConfig = {
+    clientId: `0oa7yu7ujfUbDb59Y5d7`,
+    issuer: 'https://dev-95608205.okta.com/oauth2/default',
+    redirectUrl: `http://localhost:3000/login/callback`,
+    scopes: ['openid', 'profile', 'email'],
+    pkce: true,
+    disableHttpsCheck: true
+}
